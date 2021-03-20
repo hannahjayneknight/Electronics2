75d789b071cf3ce876c5e8eab53f241b209e5743
Hannah Jayne Knight <hannah@tinpot.co.uk>
2021-03-20 22:07:17 +0000

    displaying pitch and roll angle

--- Lab7-SelfBalancingSegway/lab4task5-edited.py
+++ Lab7-SelfBalancingSegway/lab4task5-edited.py
@@ -1,11 +1,13 @@
 '''    Marks the start of comment section
 -------------------------------------------------------
-Name: User's own program for Lab 3 Exercise 5
-Creator:  	Peter YK Cheung
-Date:   	23 Dec 2017
+Name: Editing Lab 4 Exercise 5 in preparation for the oral exam
+Creator:  	Hannah Knight
+Date:   	10 March 2021
 Revision: 	1
 -------------------------------------------------------
-Put comments here to explain what this does
+Measures the pitch and roll angle, passes them through a 
+complementary filter and displays this on the OLED screen in 
+degrees. 
 -------------------------------------------------------
 '''    
 import pyb
@@ -25,21 +27,38 @@ oled.init_display()
 # IMU connected to X9 and X10
 imu = MPU6050(1, False)    	# Use I2C port 1 on Pyboard
 
-def read_imu(dt):
+def read_imu_pendulum(dt):
 	global g_pitch
 	alpha = 0.7    # larger = longer time constant
-	pitch = int(imu.pitch()) # pitch mesaured using accelerometer
-	roll = int(imu.roll())
+	pitch = int(imu.pitch())  # pitch mesaured using accelerometer
 	g_pitch = alpha*(g_pitch + imu.get_gy()*dt*0.001) + (1-alpha)*pitch # derived pitch using gyro
-	# show graphics
+	# show pendulums
 	oled.clear()
 	oled.line(96, 26, pitch, 24, 1) # function defined in the oled_938.py file: line(self,x,y,phi,d, state) -> Draws a line of lenght d from (x,y) at angle phi in degrees
-	oled.line(32, 26, g_pitch, 24, 1)
+	oled.line(32, 26, g_pitch, 24, 1) # g_pitch is more accurate
 	oled.draw_text(0,0," Raw | PITCH |")
 	oled.draw_text(83,0, "filtered")
 	oled.display()
-		
-g_pitch = 0   
+
+def read_imu(dt):
+	# prints the pitch and roll angles to the screen
+	# both have been passed through a complementary filter
+	global g_pitch
+	global g_roll
+	alpha = 0.7    # larger = longer time constant
+	pitch = int(imu.pitch())  # pitch mesaured using accelerometer - measured in degrees
+	roll = int(imu.roll() + 15) # measured in degrees
+	g_pitch = alpha*(g_pitch + imu.get_gy()*dt*0.001) + (1-alpha)*pitch # derived pitch using gyro
+	g_roll = alpha*(g_roll + imu.get_gx()*dt*0.001) + (1-alpha)*roll
+	# show text
+	oled.clear()
+	oled.draw_text(0,20,"Roll angle: {:.2f}".format(g_roll))
+	oled.draw_text(0,40, "Pitch angle: {:.2f}".format(g_pitch))
+	oled.draw_text(0,0, "(All measured in degrees)")
+	oled.display()
+
+g_pitch = 0
+g_roll = 0 
 tic = pyb.millis()		
 while True:
 	b_LED.toggle()
